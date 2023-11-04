ed071de2ca936a42bf52aae996330edcdd730980
Lucian Earl <61768165+LucianEarl@users.noreply.github.com>
2023-11-05 09:30:42 +1300

    Update margins

--- app/page.js
+++ app/page.js
@@ -4,13 +4,13 @@ import TextBox from "./components/TextBox";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12 bg-[#223C25]">
-      <h1 className="text-[#E982A2] italic font-extrabold text-6xl md:text-8xl mt-4 text-center">
+      <h1 className="text-[#E982A2] italic font-extrabold text-6xl md:text-8xl mt-4 text-center mb-9">
         Lucian Earl
       </h1>
-      <h2 className="text-[#96E072] text-4xl md:text-5xl my-9">
+      <h2 className="text-[#96E072] text-4xl md:text-5xl mb-9">
         Welcome to my silly little website
       </h2>
-      <div className="rounded-full overflow-hidden opacity-75 ">
+      <div className="rounded-full overflow-hidden opacity-75 mb-9">
         <Image src="/faceBW.png" width="384" height="384" alt="My face" />
       </div>
       <div className="flex flex-col md:flex-row justify-between md:w-4/5">
