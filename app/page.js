a560b089beefb4000c6d5f8a0628c71575765ea2
LucianEarl <lucian.earl96@gmail.com>
2023-11-27 12:07:07 +1300

    animated name

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Image from "next/image";
 import TextBox from "./components/TextBox";
 
@@ -6,13 +6,27 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className="flex min-h-screen flex-col items-center p-12 bg-[#223C25]">
-        <h1 className="text-[#E982A2] italic font-extrabold text-6xl md:text-8xl mt-4 text-center mb-9">
+        <h1 className="bg-gradient-to-r from-l-pink via-mid-pink to-l-pink text-transparent bg-300% bg-clip-text italic font-extrabold text-6xl md:text-8xl mt-4 text-center mb-9 animate-shiny">
           Lucian Earl
         </h1>
         <h2 className="text-[#96E072] text-4xl md:text-5xl mb-9">
@@ -26,7 +40,10 @@ export default function Home() {
             title="Who am I?"
             text="I’m some guy in Christchruch. Sometimes I make websites and other stuff."
           />
-          <TextBox title="What else?" text="80s music, dinosaur trivia and a bad sense of humour." />
+          <TextBox
+            title="What else?"
+            text="80s music, dinosaur trivia and a bad sense of humour."
+          />
         </div>
       </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "l-pink": "#E982A2",
+        "mid-pink": "#F4BECF",
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        shiny: "shine 8s linear infinite",
+      },
+      keyframes: {
+        shine: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
       },
     },
   },
   plugins: [],
-}
+};
